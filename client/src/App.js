3d56c713ab4385c7c52e5d8bd19e9e64785fb0f1
jeff <jeffhogg86@gmail.com>
2021-02-27 11:16:35 -0800

    update docker pw

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Docker
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
